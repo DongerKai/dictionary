0478dc01b005f221c3e8a8d6215d52ff31aee01d
dongerkai <koekchan@gmail.com>
2018-11-15 09:04:03 +0800

    初始化项目

---
+++ src/main/java/com/example/demo/configuration/SwaggerConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.demo.configuration;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Objects;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+    @Value("${spring.profiles.active}")
+    private String active;
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(!StringUtils.equals(active, "prod"))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(path-> Objects.equals(path, "/error"))
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("demo")
+                .description("demo")
+                .version("1.0.0")
+                .build();
+    }
+}
