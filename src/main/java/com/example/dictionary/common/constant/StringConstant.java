82605bd45ddfb2b7abee9db555f27556a482b804
dongerkai <koekchan@gmail.com>
2018-11-20 15:43:50 +0800

    统一返回b模板

---
+++ src/main/java/com/example/dictionary/common/constant/StringConstant.java
@@ -0,0 +1,8 @@
+package com.example.dictionary.common.constant;
+
+public class StringConstant {
+    private StringConstant(){}
+
+    public static final String DATE_FORMAT_01 = "yyyy-HH-dd HH:mm:ss";
+    public static final String GMT_8 = "GMT+8";
+}
